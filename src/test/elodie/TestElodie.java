674243502035501726e9415dc349183e1cc4e89e
Verlaine Kouwe <verlaine_k@outlook.com>
2021-01-28 19:41:37 +0100

    creation du depot

---
+++ src/test/elodie/TestElodie.java
@@ -0,0 +1,17 @@
+
+package test.elodie;
+
+/**
+ *
+ * @author verla
+ */
+public class TestElodie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hi! "+ (54*2) + " It's me!!!");
+    }
+    
+}
